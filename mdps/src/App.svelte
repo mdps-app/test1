9954a90f72a495ae4977b9a5ea2e06538483084e
shiki <kishintorii@gmail.com>
2024-03-11 11:02:54 +0900

    📁 save

--- mdps/src/App.svelte
+++ mdps/src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	export let name;
+	import Head from "./head.svelte";
 </script>
 
 <main>
+	<Head></Head>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
