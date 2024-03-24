a042bbdb219779496e7c1c2d45eddd13141bc75d
shiki <kishintorii@gmail.com>
2024-03-24 11:50:49 +0900

    📁 save
    一旦セーブ

--- mdps-hp/src/app.d.ts
+++ mdps-hp/src/app.d.ts
@@ -1,13 +1,17 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+    namespace App {
+        // interface Error {}
+        interface Locals {
+            user: {
+                id: string,
+                name: string
+            }
+        }
+        // interface PageData {}
+        // interface Platform {}
+    }
 }
 
 export {};
